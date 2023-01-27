181e767b4fc84f896f7a256e7a64b58a8b930532
aaliaksandrau <a.aliaksandrau@elinext.com>
2023-01-27 12:30:30 +0400

    fix: posts pass unit tests

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -2,11 +2,15 @@ import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-sc
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { createPostBodySchema, changePostBodySchema } from './schema';
 import type { PostEntity } from '../../utils/DB/entities/DBPosts';
+import { StatusCodes } from '../../constants/statusCodes';
+import uuid = require('uuid');
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {});
+  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {
+    return fastify.db.posts.findMany()
+  });
 
   fastify.get(
     '/:id',
@@ -15,7 +19,23 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity | null> {
+      const postId = request.params.id
+
+      if (typeof postId === 'string' && uuid.validate(postId)) {
+        const post = await fastify.db.posts.findOne({ key: 'id', equals: postId })
+
+        if (!post) {
+          reply.statusCode = StatusCodes.NOT_FOUND
+          return null
+        }
+
+        return post
+      }
+
+      reply.statusCode = StatusCodes.NOT_FOUND
+      return null
+    }
   );
 
   fastify.post(
@@ -25,7 +45,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: createPostBodySchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity> {
+      return fastify.db.posts.create({ ...request.body })
+    }
   );
 
   fastify.delete(
@@ -35,7 +57,23 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity | null> {
+      const postId = request.params.id
+
+      if (typeof postId === 'string' && uuid.validate(postId)) {
+        const post = await fastify.db.posts.findOne({ key: 'id', equals: postId })
+
+        if (!post) {
+          reply.statusCode = StatusCodes.NOT_FOUND
+          return null
+        }
+
+        return fastify.db.posts.delete(request.params.id)
+      }
+
+      reply.statusCode = StatusCodes.BAD_REQUEST
+      return null
+    }
   );
 
   fastify.patch(
@@ -46,7 +84,23 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<PostEntity> {}
+    async function (request, reply): Promise<PostEntity | null> {
+      const postId = request.params.id
+
+      if (typeof postId === 'string' && uuid.validate(postId)) {
+        const post = await fastify.db.posts.findOne({ key: 'id', equals: postId })
+
+        if (!post) {
+          reply.statusCode = StatusCodes.NOT_FOUND
+          return null
+        }
+
+        return fastify.db.posts.change(postId, request.body)
+      }
+
+      reply.statusCode = StatusCodes.BAD_REQUEST
+      return null
+    }
   );
 };
 
