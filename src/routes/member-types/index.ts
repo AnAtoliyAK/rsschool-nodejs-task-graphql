cac460b7608d20a44ce0b21342d2648c75684aee
aaliaksandrau <a.aliaksandrau@elinext.com>
2023-01-27 12:29:12 +0400

    fix: member types pass unit tests

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -2,13 +2,16 @@ import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-sc
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { changeMemberTypeBodySchema } from './schema';
 import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
+import { StatusCodes } from '../../constants/statusCodes';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
   fastify.get('/', async function (request, reply): Promise<
     MemberTypeEntity[]
-  > {});
+  > {
+    return fastify.db.memberTypes.findMany()
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +20,25 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity | null> {
+
+      const memberId = request.params.id
+
+      if (typeof memberId === 'string') {
+        const member = await fastify.db.memberTypes.findOne({ key: 'id', equals: request.params.id })
+       
+        if (!member) {
+          reply.statusCode = StatusCodes.NOT_FOUND
+
+          return null
+        }
+
+        return member
+      } 
+      reply.statusCode = StatusCodes.NOT_FOUND
+
+      return null
+    }
   );
 
   fastify.patch(
@@ -28,7 +49,25 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity | null> {
+      const memberId = request.params.id
+
+      if (typeof memberId === 'string') {
+        const member = await fastify.db.memberTypes.findOne({ key: 'id', equals: memberId })
+       
+        if (!member) {
+          reply.statusCode = StatusCodes.BAD_REQUEST
+
+          return null
+        }
+
+        return fastify.db.memberTypes.change(memberId, request.body)
+      } 
+
+      reply.statusCode = StatusCodes.BAD_REQUEST
+      
+      return null
+    }
   );
 };
 
