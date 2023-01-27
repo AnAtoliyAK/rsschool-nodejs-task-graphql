422bdb4d602a7697a95b3598b4b0e39c58817e84
aaliaksandrau <a.aliaksandrau@elinext.com>
2023-01-27 13:33:28 +0400

    fix: profiles unit tests

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -2,13 +2,15 @@ import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-sc
 import { idParamSchema } from '../../utils/reusedSchemas';
 import { createProfileBodySchema, changeProfileBodySchema } from './schema';
 import type { ProfileEntity } from '../../utils/DB/entities/DBProfiles';
+import { StatusCodes } from '../../constants/statusCodes';
+import uuid = require('uuid');
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
-    ProfileEntity[]
-  > {});
+  fastify.get('/', async function (request, reply): Promise<ProfileEntity[]> {
+    return fastify.db.profiles.findMany()
+  });
 
   fastify.get(
     '/:id',
@@ -17,7 +19,22 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity | null> {
+      const profileId = request.params.id
+
+      if (typeof profileId === 'string' && uuid.validate(profileId)) {
+        const profile = await fastify.db.profiles.findOne({ key: 'id', equals: profileId })
+
+        if (!profile) {
+          reply.statusCode = StatusCodes.NOT_FOUND
+          return null
+        }
+
+        return profile
+      }
+      reply.statusCode = StatusCodes.NOT_FOUND
+      return null
+    }
   );
 
   fastify.post(
@@ -27,7 +44,26 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         body: createProfileBodySchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity | null> {
+      const { userId, memberTypeId } = request.body
+
+      if (uuid.validate(userId) && memberTypeId) {
+        const member = await fastify.db.memberTypes.findOne({ key: 'id', equals: memberTypeId })
+        const profile = await fastify.db.profiles.findOne({ key: 'userId', equals: userId })
+
+        if (!member || profile?.userId === userId) {
+          reply.statusCode = StatusCodes.BAD_REQUEST
+
+          return null
+        }
+
+        return fastify.db.profiles.create({ ...request.body })
+      } else {
+        reply.statusCode = StatusCodes.BAD_REQUEST
+
+        return null
+      }
+    }
   );
 
   fastify.delete(
@@ -37,7 +73,23 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity | null> {
+      const profileId = request.params.id
+
+      if (typeof profileId === 'string' && uuid.validate(profileId)) {
+        const profile = await fastify.db.profiles.findOne({ key: 'id', equals: profileId })
+
+        if (!profile) {
+          reply.statusCode = StatusCodes.NOT_FOUND
+          return null
+        }
+
+        return fastify.db.profiles.delete(request.params.id)
+      }
+
+      reply.statusCode = StatusCodes.BAD_REQUEST
+      return null
+    }
   );
 
   fastify.patch(
@@ -48,7 +100,23 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<ProfileEntity> {}
+    async function (request, reply): Promise<ProfileEntity | null> {
+      const profileId = request.params.id
+
+      if (typeof profileId === 'string' && uuid.validate(profileId)) {
+        const profile = await fastify.db.profiles.findOne({ key: 'id', equals: profileId })
+
+        if (!profile) {
+          reply.statusCode = StatusCodes.NOT_FOUND
+          return null
+        }
+
+        return fastify.db.profiles.change(profileId, request.body)
+      }
+
+      reply.statusCode = StatusCodes.BAD_REQUEST
+      return null
+    }
   );
 };
 
